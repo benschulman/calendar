955049c28f5db004ce1a3fdd365b372b5f910c5a
benschulman <bgschulman31@gmail.com>
2019-04-29 22:07:27 -0500

    deleted vim genereated file and added month functions to planner.cpp

--- src/planner.cpp
+++ src/planner.cpp
@@ -10,6 +10,8 @@
 ///////////////////////////////////////////////////////////////////////////////
 #include <iostream>
 #include <time.h>
+#include <stdlib.h>
+#include <stdio.h>
 
 struct Event {
 	time_t start;
@@ -43,6 +45,89 @@ int dayNumber(int day, int month, int year) {
              year/400 + t[month-1] + day) % 7;
 }
 
-int main() {
-	std::cout << dayNumber(29, 4, 2019) << std::endl;
+/*
+ * A Function that returns a the name of the month given the number
+ *
+ * Number	Name
+ * 1		January
+ * 2		February
+ * 3		March
+ * 4		April
+ * 5		May
+ * 6		June
+ * 7		July
+ * 8		August
+ * 9		September
+ * 10		October
+ * 11		November
+ * 12		December
+ */
+std::string getNameOfMonth(int month) {
+	if(month == 1) return "January";
+	else if(month == 2) return "February";
+	else if(month == 3) return "March";
+	else if(month == 4) return "April";
+	else if(month == 5) return "May";
+	else if(month == 6) return "June";
+	else if(month == 7) return "July";
+	else if(month == 8) return "August";
+	else if(month == 9) return "September";
+	else if(month == 10) return "October";
+	else if(month == 11) return "November";
+	else if(month == 12) return "December";
+	else {perror("Invalid Month"); exit(1);}
+}
+
+/*
+ * A function that returns the number of days given a month (number) and year
+ *
+ * Month	Days
+ * 1		31
+ * 2		28/29 If the year is divisible by 400 or if the year is divisible
+ * 				  by 4 and not 100 then it is a leap year otherwise it is not
+ * 3		31
+ * 4		30
+ * 5		31
+ * 6		30
+ * 7		31
+ * 8		31
+ * 9		30
+ * 10		31
+ * 11		30
+ * 12		31
+ *
+ * If an invalid month is entered then 0 is returned
+ */
+int getNumDays(int month, int year) {
+	if(month == 1) return 31;
+	else if(month == 2) {
+		if(year % 400 == 0 || (year % 4 == 0 && year % 100 != 0))
+			return 29;
+		else
+			return 28;
+	}
+	else if(month == 3) return 31;
+	else if(month == 4) return 30;
+	else if(month == 5) return 31;
+	else if(month == 6) return 30;
+	else if(month == 7) return 31;
+	else if(month == 8) return 31;
+	else if(month == 9) return 30;
+	else if(month == 10) return 31;
+	else if(month == 11) return 30;
+	else if(month == 12) return 31;
+	else return 0;
+	std::cout << "Not a valid month!" << std::endl;
+}
+
+/*
+ * A function that prints the calendar for a given month and year
+ */
+void printCalendar(int month, int year) {
+	std::cout << "\t\t" << getNameOfMonth(month) << " - " << year << std::endl;
+	//TODO Finish method
+}
+
+int main(int argc, char* argv[]) {	
+	printCalendar(atoi(argv[1]), atoi(argv[2]));
 }
