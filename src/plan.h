c107f403a3c2ab97277046d114bab94d5daa82e0
benschulman <bgschulman31@gmail.com>
2019-05-04 14:06:00 -0500

    added header file and updated .gitignore

---
+++ src/plan.h
@@ -0,0 +1,17 @@
+#ifndef PLAN_H
+	struct Event;
+	bool compareEvents(Event e1, Event e2);
+	int dayNumber(int day, int month, int year);
+	std::string getNameOfMonth(int month);
+	int getNumDays(int month, int year);
+	void printCalendar(int month, int year);
+	void printUsage();
+	void printEvent(const Event& e);
+	void printEvents(const std::vector<Event>& events);
+	void parseEvents(const std::string& filepath, std::vector<Event>* events);
+	void removeEvent(const std::string& title, std::vector<Event>* events);
+	std::string getFilepath(const Event& e);
+	std::vector<Event>addEvent(Event& e);
+	void writeEvent(std::ofstream& out, const Event& e);
+	void overwriteFile(const std::string& filepath, const std::vector<Event>& events);
+#endif
